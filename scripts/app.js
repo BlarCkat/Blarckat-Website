29cb17e0775e14308c3576f7a6a9e5ff99907785
John St. Aïmond Banson <articwolf177@gmail.com>
2022-07-26 15:14:40 -0700

    Light and Dark Theme

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,59 @@
 
+
+
+// Side Navigation
+const sideNav = document.getElementById('sideNav');
+const navOverlay = document.getElementById('nav-overlay');
+
 function openNav(){
-    document.getElementById('sideNav').style.width = '50vw';
+    sideNav.style.visibility = 'visible';
+    sideNav.style.width = '50vw';
+    navOverlay.style.width = '100vw';
+    console.log(navOverlay.style.width)
 }
 function closeNav(){
-    document.getElementById('sideNav').style.width = '0';
+    sideNav.style.visibility = 'hidden';
+    sideNav.style.width = '0';
+    navOverlay.style.width = '0';
+
+    console.log(navOverlay.style.width)
 }
+// ///////////////////////////////////////////////////
+
+
+// Light and Dark Mode
+let darkMode = localStorage.getItem('darkMode');
+let lightMode = localStorage.getItem('lightMode');
+const darkModeToggle = document.querySelector('#themeToggle');
+
+
+const enableDarkMode = ()=>{
+    document.body.classList.add('dark-mode');
+    document.body.classList.remove('light-mode');
+    localStorage.setItem('darkMode', 'enabled');
+    localStorage.setItem('lightMode', 'disabled');
+}
+
+const enableLightMode = ()=>{
+    document.body.classList.add('light-mode');
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('lightMode', 'enabled');
+    localStorage.setItem('darkMode', 'disabled');
+}
+
+if (darkMode === 'enabled') {
+    enableDarkMode();
+} else{
+    enableLightMode();
+}
+
+// When theme button is clicked
+darkModeToggle.addEventListener('click', ()=> {
+    darkMode = localStorage.getItem('darkMode');
+    lightMode = localStorage.getItem('lightMode');
+    if (darkMode !== 'enabled') {
+        enableDarkMode();
+    } else {
+        enableLightMode();
+    }
+})
