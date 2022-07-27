d57986e66826fb7602f601defdf2cd7d8cd7d860
John St. Aïmond Banson <articwolf177@gmail.com>
2022-07-27 00:32:14 -0700

    Side Navigation

--- scripts/app.js
+++ scripts/app.js
@@ -1,31 +1,25 @@
-
-
-
 // Side Navigation
 const sideNav = document.getElementById('sideNav');
 const navOverlay = document.getElementById('nav-overlay');
 
 function openNav(){
+    // show navbar, make it fit 50% of the screen width, and make the overlay fit 100%, 
     sideNav.style.visibility = 'visible';
     sideNav.style.width = '50vw';
     navOverlay.style.width = '100vw';
-    console.log(navOverlay.style.width)
 }
 function closeNav(){
+    // hide side navigation, reset everything to 0.
     sideNav.style.visibility = 'hidden';
     sideNav.style.width = '0';
     navOverlay.style.width = '0';
-
-    console.log(navOverlay.style.width)
 }
 // ///////////////////////////////////////////////////
-
 
 // Light and Dark Mode
 let darkMode = localStorage.getItem('darkMode');
 let lightMode = localStorage.getItem('lightMode');
 const darkModeToggle = document.querySelector('#themeToggle');
-
 
 const enableDarkMode = ()=>{
     document.body.classList.add('dark-mode');
@@ -57,3 +51,41 @@ darkModeToggle.addEventListener('click', ()=> {
         enableLightMode();
     }
 })
+
+
+// Smooth Scrolling
+// let current = 0;
+// let target = 0;
+// let ease = 0.15;
+
+// let windowWidth;
+// let containerHeight;
+// let imageHeight;
+// let skewDifference;
+
+// let smoothScrollContainer = document.querySelector('.smoothScroll');
+
+// function lerp(animationStart, animationEnd, t) {
+//     return animationStart * (1 - t) + animationEnd * t;
+// }
+
+// function setTransform(el, transform) {
+//     el.style.transform = transform;
+// }
+
+// function setupSmoothScroll() {
+//     windowWidth =  window.innerWidth;
+//     containerHeight =  smoothScrollContainer.getBoundingClientRect().height;
+//     document.body.style.height = `${containerHeight}px`;
+// }
+
+// function smoothScrollAnimation() {
+//     current = lerp(current, target, ease);
+//     current = parseFloat(current.toFixed(2));
+//     target = window.scrollY
+
+//     setTransform(smoothScrollContainer, `translateY(${-current}px)`);
+//     requestAnimationFrame(smoothScrollAnimation);
+// }
+
+// setupSmoothScroll()
